2ef44e29ed820cf8554fa39bb24d8f98c2bf9d73
Pankhaniya Dhruv <vijyapankhaniya1968@gmail.com>
2026-08-09 18:37:36 +0530

    Solve core origine

--- server/main.js
+++ server/main.js
@@ -13,7 +13,11 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
